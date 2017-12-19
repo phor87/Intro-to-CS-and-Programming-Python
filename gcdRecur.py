2b751dbc93ee3fa40c9994f062cc27aa616877c5
Paul Hor <phor87@gmail.com>
2017-12-19 11:07:17 -0800

    Week 2: Simple Programs Exercise: bcd recur

---
+++ gcdRecur.py
@@ -0,0 +1,13 @@
+def gcdRecur(a, b):
+    '''
+    a, b: positive integers
+
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    # Your code here
+    if b == 0:
+        return a
+    else:
+        return gcdRecur(b, a % b)
+
+print(gcdRecur(10,4))
